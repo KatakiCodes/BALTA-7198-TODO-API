fcf23f882ceee44de47ba163cbfd1ba543f22297
= <=>
2025-06-11 12:24:54 -0300

    feat: criando a entidade TodoItem

---
+++ TODO.Domain/Entities/TodoItem.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace TODO.Domain.Entities;
+
+public class TodoItem : Entity
+{
+    public TodoItem(string title, DateTime date, string user)
+    {
+        Title = title;
+        Done = false;
+        Date = date;
+        User = user;
+    }
+
+    public string Title { get; private set; } = string.Empty;
+    public bool Done { get; private set; }
+    public DateTime Date { get; private set; }
+    public string User { get; private set; } = string.Empty;
+
+    public void MarkAsDone()
+    {
+
+    }
+    public void MarkAsUndone()
+    {
+        
+    }
+}
